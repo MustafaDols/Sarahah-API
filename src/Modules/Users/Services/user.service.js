84cd01636f891d5aae8a19687edbd576fdb32ecb
MustafaDols <mustafadolseka491@gmail.com>
2025-08-21 02:01:27 +0300

    add JWT authentication middleware with token verification

--- src/Modules/Users/Services/user.service.js
+++ src/Modules/Users/Services/user.service.js
@@ -1,92 +1,95 @@
-import { hashSync } from "bcrypt";
+import { compareSync , hashSync } from "bcrypt";
 import User from "../../../DB/Models/user.model.js";
+import BlackListedTokens from "../../../DB/Models/black-listed-tokens.model.js";
 import { assymetricDecryption, assymetricEncryption, encrypt, decrypt } from "../../../Utils/encryption.utils.js";
-import { sendEmail } from "../../../Utils/send-email.utils.js";
-import { compareSync } from "bcrypt";
 import { customAlphabet } from "nanoid";
 import { emitter } from "../../../Utils/send-email.utils.js"
+import { v4 as uuidv4 } from "uuid"
+import { generateToken , verifyToken } from "../../../Utils/tokens.utils.js";
 
 const uniqueString = customAlphabet('1234567890abcdef', 5)
+
+ 
 export const signUpService = async (req, res) => {
-    
-        const { firstname, lastname, email, password, age, gender, phoneNumber } = req.body
 
-        const isUserExist = await User.findOne({
-            $or: [
-                { email },
-                { firstname, lastname }
-            ]
-        })
+    const { firstname, lastname, email, password, age, gender, phoneNumber } = req.body
 
-        if (isUserExist) {
-            return res.status(409).json({ message: "User already exists" });
-        }
+    const isUserExist = await User.findOne({
+        $or: [
+            { email },
+            { firstname, lastname }
+        ]
+    })
 
-        //Encrypt phone number
-        const encryptedPhoneNumber = assymetricEncryption(phoneNumber)
+    if (isUserExist) {
+        return res.status(409).json({ message: "User already exists" });
+    }
 
-        //Hash password
-        const hashedPassword = hashSync(password, +process.env.SALT_ROUNDS)
+    //Encrypt phone number
+    const encryptedPhoneNumber = assymetricEncryption(phoneNumber)
 
-        const otp = uniqueString()
+    //Hash password
+    const hashedPassword = hashSync(password, +process.env.SALT_ROUNDS)
 
-        const user = await User.create({
-            firstname,
-            lastname,
-            email,
-            password: hashedPassword,
-            age,
-            gender,
-            phoneNumber: encryptedPhoneNumber,
-            otps: { confirmation: hashSync(otp, +process.env.SALT_ROUNDS) }
-        })
+    const otp = uniqueString()
 
-        //Send email for registred user
-        // await sendEmail({
-        //     to: email,
-        //     subject: "Confirmation email",
-        //     content: ` Your confirmation otp is ${otp} `,
-        //     attachments: [
-        //         {
-        //             filename: "confirmation.png",
-        //             path: "confirmation.png"
-        //         }
-        //     ]
-        // })
+    const user = await User.create({
+        firstname,
+        lastname,
+        email,
+        password: hashedPassword,
+        age,
+        gender,
+        phoneNumber: encryptedPhoneNumber,
+        otps: { confirmation: hashSync(otp, +process.env.SALT_ROUNDS) }
+    })
 
-        emitter.emit('sendEmail', {
-            to: email,
-            subject: "Confirmation email",
-            content: ` Your confirmation otp is ${otp} `,
-            attachments: [
-                {
-                    filename: "confirmation.png",
-                    path: "confirmation.png"
-                }
-            ]
-        })
+    //Send email for registred user
+    // await sendEmail({
+    //     to: email,
+    //     subject: "Confirmation email",
+    //     content: ` Your confirmation otp is ${otp} `,
+    //     attachments: [
+    //         {
+    //             filename: "confirmation.png",
+    //             path: "confirmation.png"
+    //         }
+    //     ]
+    // })
 
-        // const userInstance = new User({ firstname, lastname, email, password, age, gender })
-        // const user = await userInstance.save()
-        return res.status(201).json({ message: "User created successfully", user })
+    emitter.emit('sendEmail', {
+        to: email,
+        subject: "Confirmation email",
+        content: ` Your confirmation otp is ${otp} `,
+        attachments: [
+            {
+                filename: "confirmation.png",
+                path: "confirmation.png"
+            }
+        ]
+    })
+
+    // const userInstance = new User({ firstname, lastname, email, password, age, gender })
+    // const user = await userInstance.save()
+    return res.status(201).json({ message: "User created successfully", user })
 }
 
 
 export const confirmEmailService = async (req, res, next) => {
 
     const { email, otp } = req.body
-    const user = await User.findOne({ email , isConfirmed: false }) 
+    const user = await User.findOne({ email, isConfirmed: false })
 
     if (!user) {
         // return res.status(404).json({ message: "User not found or already confirmed"  });
         return next(new Error("User not found or already confirmed", { cause: 400 }))
     }
-    
+
     const isOtpMatch = compareSync(otp, user.otps?.confirmation)
     if (!isOtpMatch) {
-        return res.status(404).json({ message: "Invalid otp"  });
+        return res.status(404).json({ message: "Invalid otp" });
     }
-    
+
     user.isConfirmed = true
     user.otps.confirmation = undefined
 
@@ -97,71 +100,89 @@ export const confirmEmailService = async (req, res, next) => {
 
 export const signinService = async (req, res) => {
 
-        const { email, password } = req.body
-        const user = await User.findOne({ email })
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
 
-        if (!user) {
-            return res.status(404).json({ message: "Invalid email or password" });
+    if (!user) {
+        return res.status(404).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordMatch = compareSync(password, user.password)
+
+    if (!isPasswordMatch) {
+        return res.status(404).json({ message: "Invalid email or password" });
+    }
+
+    // Generate token for logged in user
+    const accesstoken = generateToken(
+        { _Id: user._id, email: user.email },
+        process.env.JWT_ACCESS_SECRET,
+        {
+            // issuer: 'https://localhost:3000',
+            // audience: 'https://localhost:4000',
+            expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
+            jwtid: uuidv4()
         }
-
-        const isPasswordMatch = compareSync(password, user.password)
-
-        if (!isPasswordMatch) {
-            return res.status(404).json({ message: "Invalid email or password" });
-        }
-        return res.status(200).json({ message: "User signed in successfully", user })
+    )
+    return res.status(200).json({ message: "User signed in successfully", accesstoken })
 }
 
 
 export const updateAccountService = async (req, res) => {
 
-        const { userId } = req.params
-        const { firstname, lastname, email, age, gender } = req.body
-        const user = await User.findById(userId)
+    const { _Id } = req.loggedInUser
+    const { firstname, lastname, email, age, gender } = req.body
 
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        if (firstname) user.firstname = firstname
-        if (lastname) user.lastname = lastname
-        if (email) {
-            const isEmailExist = await User.findOne({ email })
-            if (isEmailExist) {
-                return res.status(409).json({ message: "Email already exists" });
-            }
-            user.email = email
-
-        }
-        if (age) user.age = age
-        if (gender) user.gender = gender
-
-        await user.save()
-        return res.status(200).json({ message: "User updated successfully", user })
+    const user = await User.findByIdAndUpdate(
+        _Id,
+        { firstname, lastname, email, age, gender },
+        { new: true }
+    )
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    // await user.save()
+    return res.status(200).json({ message: "User updated successfully" })
 }
 
 
 export const deleteAccountService = async (req, res) => {
 
-        const { userId } = req.params
-        const deletedResult = await User.deleteOne({ _id: userId })
+    const { userId } = req.params
+    const deletedResult = await User.deleteOne({ _id: userId })
 
-        if (!deletedResult.deletedCount) {
-            return res.status(404).json({ message: "User not found" });
-        }
-        return res.status(200).json({ message: "User deleted successfully" })
+    if (!deletedResult.deletedCount) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json({ message: "User deleted successfully" })
 }
 
 
 export const listUsersService = async (req, res) => {
-    
-        let users = await User.find()
 
-        users = users.map((user) => {
-            return {
-                ...user._doc,
-                phoneNumber: assymetricDecryption(user.phoneNumber)
-            }
-        })
-        return res.status(200).json({ message: "Users fetched successfully", users })
+    let users = await User.find()
+
+    users = users.map((user) => {
+        return {
+            ...user._doc,
+            phoneNumber: assymetricDecryption(user.phoneNumber)
+        }
+    })
+    return res.status(200).json({ message: "Users fetched successfully", users })
+}
+
+
+export const LogoutService = async (req, res) => {
+ 
+    const {accessToken} = req.headers
+    const decodedData = verifyToken(accessToken, process.env.JWT_ACCESS_SECRET)
+
+    const expirationDate = new Date(decodedData.exp*1000)
+
+    const blackListedToken = await BlackListedTokens.create({
+        tokenId: decodedData.jti,
+        expirationDate
+    })
+
+    return res.status(200).json({ message: "User logged out successfully" })
 }
--- src/Modules/Users/user.controller.js
+++ src/Modules/Users/user.controller.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import * as userServices from "./Services/user.service.js";
+import { authenticationMiddleware } from "../../Middlewares/authentication.middleware.js";
 const router = Router();
 
 
 router.post("/signup", userServices.signUpService);
 router.post("/signin", userServices.signinService);
-router.put("/update/:userId", userServices.updateAccountService);
+router.post("/logout", userServices.LogoutService);
+router.put("/update", authenticationMiddleware , userServices.updateAccountService);
 router.delete("/delete/:userId", userServices.deleteAccountService);
 router.get("/list", userServices.listUsersService);
-router.put("/confirm",userServices.confirmEmailService) 
- 
+router.put("/confirm", userServices.confirmEmailService)
+
 export default router;
 
 
